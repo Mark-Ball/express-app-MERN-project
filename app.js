5d1d6624b5d67c37ec2a735b6a0f74375c60e97c
Mark-Ball <markball111@gmail.com>
2020-01-21 16:22:50 +1100

    added cors middleware

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const routes = require('./routes/');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
