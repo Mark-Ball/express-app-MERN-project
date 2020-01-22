2e248bf30c3ce33ebfdbad58e6ff0fef8d0ee4f6
Mark-Ball <markball111@gmail.com>
2020-01-23 10:36:18 +1100

    changed logic of creating JWT in users controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,20 +1,35 @@
 const UserModel = require('./../database/models/user_model');
 const jwt = require('jsonwebtoken');
 
+// create a JWT based on a payload related to the user and the secret
+function createJWT(payload) {
+    return jwt.sign({ sub: payload }, process.env.JWT_SECRET);
+}
+
 // write new user to database
 async function registerUser(req, res) {
     try {
+        // destructuring used to create email and password variables from req.body
         const { email, password } = req.body;
-        await UserModel.create({ email, password, approved: false });
-        res.sendStatus('200');
+        // email and password variables used to create entry in user collection of database
+        const { _id } = await UserModel.create({ email, password, approved: false });
+        // create a JWT based on the id the user just created
+        const token = createJWT(_id);
+        // if successful, respond with the JWT
+        res.json(token);
+        // catch block included in case of error interfacing with MongoDB
     } catch(error) {
+        // send 400 status in case of error
         res.sendStatus('400');
     }
 }
 
 // correct login details provided, create JWT and send
 function loginSuccess(req, res) {
-    const token = jwt.sign({ sub: req.user._id }, process.env.JWT_SECRET);
+    // create the JWT using the user's unique MongoDB id and secret frm .env file
+    // const token = jwt.sign({ sub: req.user._id }, process.env.JWT_SECRET);
+    const token = createJWT(req.user._id);
+    // respond with the JWT as json
     res.json(token);
 }
 
