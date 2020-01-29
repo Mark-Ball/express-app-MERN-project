71b1c5dbc4830fb6a4920adf6cd57e48bf35906e
JC-Ferguson <jcferguson1996@gmail.com>
2020-01-29 15:33:54 +1100

    added functionality of uploading tags to mongodb and pdf file to s3 bucket

--- controllers/files_controller.js
+++ controllers/files_controller.js
@@ -1,8 +1,8 @@
 const FileModel = require('./../database/models/file_model');
 const aws = require("aws-sdk");
 require("dotenv").config();     // config dotenv to load in dotenv file
-const fs = require("fs");
 const multer = require('multer');
+const multerS3 = require("multer-s3");
 
 aws.config.update({
     region: "ap-southeast-2",
@@ -10,12 +10,15 @@ aws.config.update({
     secretAccessKey: process.env.AWSSecretKey
 })
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-    cb(null, 'public')
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname )
+// Create S3 service object
+const s3 = new aws.S3({apiVersion: '2006-03-01'});
+const s3_Bucket = process.env.Bucket;
+const storage = multerS3({
+    s3: s3,
+    bucket: s3_Bucket,
+    key: function (req,file,cb){
+        const ext = file.mimetype.split("/")[1];
+        cb(null, Date.now() + "."+ ext);
     }
 });
 
@@ -24,15 +27,15 @@ const upload = multer({ storage: storage }).single('file');
 // save file to db and s3
 function saveFile(req, res) {
 
-    upload(req, res, function (err) {
-        console.log(req.body);
+    upload(req, res, async function (err) {
+        const { name, solution, dateCreated, description, prerequisites, whoItBenefits } = req.body;
         if (err instanceof multer.MulterError) {
             return res.status(500).json(err)
         } else if (err) {
             return res.status(500).json(err)
         }
         // logic to write into MongoDB
-
+        await FileModel.create({name: name, tags: { solution: solution, createdOn: dateCreated, description: description, prerequisites: prerequisites.split(","), benefits: whoItBenefits.split(",")  }, location: "qwertv" });
       return res.status(200).send(req.file)
     })
 }
@@ -45,11 +48,6 @@ function searchFiles(req, res) {
 // retrieve single file based on selection in returned search
 async function show(req,res){
     const {key} = req.params;
-
-    const s3_Bucket = process.env.Bucket;
-
-    // Create S3 service object
-    const s3 = new aws.S3({apiVersion: '2006-03-01'});
 
     // Call S3 to get an object creates readable stream for display on the front end
     const stream = await s3.getObject({Bucket: s3_Bucket, Key: key}).createReadStream();
