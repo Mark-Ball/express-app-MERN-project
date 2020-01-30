63fc58de6d9b38a72af7470e68d8251c3f269f33
JC-Ferguson <jcferguson1996@gmail.com>
2020-01-30 11:02:48 +1100

    added new data received from frontend(proficiency,lesson content) to be added to mongodb

--- controllers/files_controller.js
+++ controllers/files_controller.js
@@ -13,12 +13,14 @@ aws.config.update({
 // Create S3 service object
 const s3 = new aws.S3({apiVersion: '2006-03-01'});
 const s3_Bucket = process.env.Bucket;
+// variable used as a unique variable name to be stored in bucket
+const fileName = Date.now();
 const storage = multerS3({
     s3: s3,
     bucket: s3_Bucket,
     key: function (req,file,cb){
         const ext = file.mimetype.split("/")[1];
-        cb(null, Date.now() + "."+ ext);
+        cb(null, fileName + "."+ ext);
     }
 });
 
@@ -28,14 +30,17 @@ const upload = multer({ storage: storage }).single('file');
 function saveFile(req, res) {
 
     upload(req, res, async function (err) {
-        const { name, solution, dateCreated, description, prerequisites, whoItBenefits } = req.body;
+        const { name, solution, dateCreated, proficiency, lessonContent, description, prerequisites, whoItBenefits } = req.body;
         if (err instanceof multer.MulterError) {
             return res.status(500).json(err)
         } else if (err) {
             return res.status(500).json(err)
         }
         // logic to write into MongoDB
-        await FileModel.create({name: name, tags: { solution: solution, createdOn: dateCreated, description: description, prerequisites: prerequisites.split(","), benefits: whoItBenefits.split(",")  }, location: "qwertv" });
+        await FileModel.create({
+            name: name, tags: { solution: solution, createdOn: dateCreated, proficiency: proficiency, content: lessonContent, 
+            description: description, prerequisites: prerequisites.split(","), benefits: whoItBenefits.split(",")  }, location: fileName + ".pdf" 
+        });
       return res.status(200).send(req.file)
     })
 }
